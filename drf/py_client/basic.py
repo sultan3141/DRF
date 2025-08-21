c2184ace6c3b23d66fa6e9dd1ce3688f07c23ce7
Sultan Adinan <Adinanyous1@gmail.com>
2025-08-21 20:12:40 +0000

    Add requests dependency and update requirements.txt

---
+++ drf/py_client/basic.py
@@ -0,0 +1,8 @@
+import requests
+
+#endpoint="https://httpbin.org/status/200/"
+endpoint="http://127.0.0.1:8000/"
+
+get_response=requests.get(endpoint, json={"query":"hello world"})
+print(get_response.text)
+print(get_response.json())
